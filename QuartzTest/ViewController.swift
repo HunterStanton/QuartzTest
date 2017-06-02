061eb3e5159a94bfe93982c3d177907b0df45cdf
Hunter_Stanton <ihatecompvir@icloud.com>
2017-06-02 09:30:05 -0400

    Initial Commit

---
+++ QuartzTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QuartzTest
+//
+//  Created by Hunter Stanton on 6/2/17.
+//  Copyright © 2017 Hunter Stanton. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
